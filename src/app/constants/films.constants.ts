2c243320fa64121d3f5d8da9c6046b9c3d10e664
Viktor <viktor.avgn@yandex.ru>
2025-09-23 00:18:05 +0700

    first commit

---
+++ src/app/constants/films.constants.ts
@@ -0,0 +1,31 @@
+import { Film } from '../models/film.model';
+
+export const FILMS: Film[] = [
+  {
+    id: 0,
+    title: 'Криминальное чтиво 1',
+    original: 'Pulp Fiction 1994',
+    category: 'Криминал/Триллер',
+    rating: 8.7,
+    link: './film.html',
+    image: './images/poster-film.png',
+  },
+  {
+    id: 1,
+    title: 'Криминальное чтиво 2',
+    original: 'Pulp Fiction 1994',
+    category: 'Криминал/Триллер',
+    rating: 8.7,
+    link: './film.html',
+    image: './images/poster-film.png',
+  },
+  {
+    id: 2,
+    title: 'Криминальное чтиво 3',
+    original: 'Pulp Fiction 1994',
+    category: 'Криминал/Триллер',
+    rating: 8.7,
+    link: './film.html',
+    image: './images/poster-film.png',
+  },
+];
